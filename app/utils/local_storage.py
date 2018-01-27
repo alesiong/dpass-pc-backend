63390e929a0186cc3d344f03aa05e2eeab48b1dc
zymop18 <115010276@link.cuhk.edu.cn>
2018-01-27 18:16:48 +0800

    Add the function 'Balance', which will return the storage space of the disk containing the current used file.

--- app/utils/local_storage.py
+++ app/utils/local_storage.py
@@ -1,3 +1,8 @@
+import ctypes
+import os
+import platform
+import sys
+
 class LocalStorage:
     def __init__(self):
         self.__blockchain = []
@@ -21,4 +26,19 @@ class LocalStorage:
         pass
 
     def balance(self) -> int:
-        pass
+        if platform.system() == 'Windows':
+            free_bytes = ctypes.c_ulonglong(0)
+            ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(sys.path[0][0:3]), None, None,
+                                                       ctypes.pointer(free_bytes))
+
+            return free_bytes.value * 8
+        else:
+            st = os.statvfs('/')
+            return st.f_bavail * st.f_frsize * 8 * 1024
+
+
+
+
+
+
+
