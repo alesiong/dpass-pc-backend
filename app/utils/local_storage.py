1a101b77e971d760e3126892c6518153e51ccacb
Yuhengli30 <731894111@qq.com>
2018-01-27 16:48:20 +0800

    Add basic framework for storage layer API

---
+++ app/utils/local_storage.py
@@ -0,0 +1,24 @@
+class LocalStorage:
+    def __init__(self):
+        self.__blockchain = []
+
+    def add(self, k: str, v: str):
+        pass
+
+    def delete(self, k: str):
+        pass
+
+    def get(self, k: str) -> str:
+        pass
+
+    def get_all(self) -> dict:
+        pass
+
+    def store(self):
+        pass
+
+    def calculate_cost(self, op: str, args: dict) -> int:
+        pass
+
+    def balance(self) -> int:
+        pass
