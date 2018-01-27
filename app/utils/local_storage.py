359b48690d9774040151bfcaea923ad1a96783fb
Yuhengli30 <731894111@qq.com>
2018-01-27 21:14:33 +0800

    Add basic framework for storage layer API

--- app/utils/local_storage.py
+++ app/utils/local_storage.py
@@ -9,10 +9,25 @@ class LocalStorage:
         pass
 
     def get(self, k: str) -> str:
-        pass
+        dic = self.get_all()
+        return dic.get(k)
+
 
     def get_all(self) -> dict:
-        pass
+        dic1 = {}
+        dic2 = {}
+        for i in range(1, len(self.__blockchain)):
+            deletion = self.__blockchain[i]
+            if deletion["operation"] == "del":
+                dic2.append(deletion["arguments"]["key"])
+
+        for i in range(1, len(self.__blockchain)):
+            element = self.__blockchain[i]
+            if element["arguments"]["key"] not in dic2:
+                dic1[element["arguments"]["key"]] = element["arguments"]["value"]
+        return dic1
+
+
 
     def store(self):
         pass
