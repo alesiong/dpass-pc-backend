bf5e57b03fb0ab49e13f7f83eb767be08bb4bc0c
alesiong <ypf7.10@hotmail.com>
2018-02-03 20:33:24 +0800

    fix color runner on windows

--- manage.py
+++ manage.py
@@ -7,7 +7,11 @@ Use "python manage.py runserver" to start the development web server on localhos
 Use "python manage.py runserver --help" for additional runserver options.
 """
 import unittest
-from colour_runner.runner import ColourTextTestRunner
+
+try:
+    from colour_runner.runner import ColourTextTestRunner as TextTestRunner
+except ImportError:
+    from unittest.runner import TextTestResult
 
 from flask_migrate import MigrateCommand
 from flask_script import Manager, Shell, Server
@@ -23,7 +27,7 @@ manager = Manager(app)
 def test():
     """Runs the unit tests without test coverage."""
     tests = unittest.TestLoader().discover('tests', pattern='test*.py')
-    result = ColourTextTestRunner(verbosity=2).run(tests)
+    result = TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
     return 1
