2f3fd9cb04ff54db26f28365289d9fad70c104c6
Yuhengli30 <731894111@qq.com>
2018-02-04 17:41:43 +0800

    Add "cipher.py"

--- app/utils/cipher.py
+++ app/utils/cipher.py
@@ -42,3 +42,18 @@ def decrypt_and_verify(ciphertext: bytes, mac: bytes, key: bytes) -> Optional[by
         return message
     except ValueError:
         return None
+from Crypto import Random
+from Crypto.Hash import SHA3_512
+
+
+def salt_hash(password: str, salt: bytes = None, iteration: int = 1000):
+    if not salt:
+        salt = Random.get_random_bytes(64)
+    password = password.encode()
+    for i in range(iteration):
+        h_obj = SHA3_512.new()
+        h_obj.update(password)
+        password = h_obj.digest()
+        if i == iteration // 2:
+            password = password + salt
+    return password, salt
