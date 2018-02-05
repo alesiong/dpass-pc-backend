dbd9e4022f0b7c339c022f0d6d948d34bfa0048b
alesiong <ypf7.10@hotmail.com>
2018-02-05 11:07:31 +0800

    finish session encryption (frontend)

--- app/frontend/app.js
+++ app/frontend/app.js
@@ -3,8 +3,28 @@ import 'mdui/dist/css/mdui.css';
 
 import type {App} from './component';
 import Component from './component';
+
 import './index';
+import {decryptAndVerify, encryptAndAuthenticate, getLocationHashValue, refreshSessionKey} from './utils';
+import mdui from 'mdui';
 
-const app: App = {};
+declare var $$: mdui.jQueryStatic;
 
-Component.init(app);
+function createApp(callback: (App) => void) {
+  let sessionKey = getLocationHashValue('key');
+  location.hash = '';
+  if (sessionKey === null) {
+    sessionKey = localStorage.getItem('sessionKey');
+    if (!sessionKey) {
+      throw Error;
+    }
+  }
+  const promise = refreshSessionKey(sessionKey);
+  promise.then((sessionKey) => {
+    callback({
+      sessionKey: sessionKey
+    });
+  });
+}
+
+createApp(Component.init);
---
+++ app/frontend/utils.js
@@ -0,0 +1,79 @@
+// @flow
+
+import CryptoJS from 'crypto-js';
+import AES from 'crypto-js/aes';
+import hmacSHA256 from 'crypto-js/hmac-sha256';
+import mdui from 'mdui';
+
+declare var $$: mdui.jQueryStatic;
+
+const fixedIV = 'Kv7OoDnecCsU11N2WTbNow==';
+
+export function getLocationHashValue(key: string): string | null {
+  const matches = location.hash.match(new RegExp(key + '=([^&]*)'));
+  return matches ? matches[1] : null;
+}
+
+export function encryptAndAuthenticate(message: string, key: string): [string, string] {
+  key = CryptoJS.enc.Hex.parse(key);
+  const cipher = AES.encrypt(message, key, {
+    iv: CryptoJS.enc.Base64.parse(fixedIV),
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7
+  }).ciphertext;
+
+  const hmac = hmacSHA256(cipher, key);
+
+  return [cipher.toString(CryptoJS.enc.Base64), hmac.toString(CryptoJS.enc.Base64)];
+}
+
+export function decryptAndVerify(ciphertext: string, hmac: string, key: string): ?string {
+  key = CryptoJS.enc.Hex.parse(key);
+  if (hmacSHA256(CryptoJS.enc.Base64.parse(ciphertext), key).toString(CryptoJS.enc.Base64) === hmac) {
+    const cipher = AES.decrypt(ciphertext, key, {
+      iv: CryptoJS.enc.Base64.parse(fixedIV),
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+    });
+    return cipher.toString(CryptoJS.enc.Utf8);
+  }
+  return null;
+}
+
+function toPromise(obj: Object) {
+  const func = obj.func;
+  return new Promise((resolve, reject) => {
+    Object.assign(obj, {
+      success: (res) => resolve(res),
+      error: (res) => reject(res)
+    });
+    func(obj);
+  });
+
+}
+
+export async function refreshSessionKey(sessionKey: string): Promise<string> {
+  const [cipher, hmac] = encryptAndAuthenticate('refresh', sessionKey);
+
+  try {
+    const res = await toPromise({
+      func: $$.ajax,
+      url: '/api/session/refresh/',
+      method: 'POST',
+      dataType: 'json',
+      data: JSON.stringify({
+        data: cipher,
+        hmac: hmac
+      }),
+      contentType: 'application/json'
+    });
+    const newSessionKey = decryptAndVerify(res.data, res.hmac, sessionKey);
+    if (newSessionKey) {
+      localStorage.setItem('sessionKey', newSessionKey);
+      return newSessionKey;
+    }
+  } catch (e) {
+  }
+
+  return sessionKey;
+}
