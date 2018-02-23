a5193ddc27ccf74799486dcef87eeb843ae027aa
sherryCPY <115010118@link.cuhk.edu.cn>
2018-02-23 22:45:01 +0800

    Modify the function get_settings() and get_table() from KeyLookupTable Modify the requirements.txt Delete demo.py

--- app/api/password.py
+++ app/api/password.py
@@ -1,6 +1,6 @@
 import base64
 import binascii
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request, json
 
 from app import SessionKey
 from app.models import KeyLookupTable
@@ -17,14 +17,14 @@ def get_table():
     key = session_key.session_key
     check_expire = current_app.config['MASTER_PASSWORD'].check_expire()
     if not check_expire:
-        hidden = current_app.config['HIDDEN']
+        hidden = request.args['hidden']
         if hidden:
-            data, hmac = encrypt_and_authenticate(KeyLookupTable.query.filter_by(hidden=False).first().encode(),
+            data, hmac = encrypt_and_authenticate(json.dumps(KeyLookupTable.query.filter_by(hidden=False)).encode(),
                                                   binascii.unhexlify(key))
             return jsonify(data=base64.encodebytes(data).decode().strip(),
                            hmac=base64.encodebytes(hmac).decode().strip())
         else:
-            data, hmac = encrypt_and_authenticate(KeyLookupTable.query.all().encode(),
+            data, hmac = encrypt_and_authenticate(json.dumps(KeyLookupTable.query.all()).encode(),
                                                   binascii.unhexlify(key))
             return jsonify(data=base64.encodebytes(data).decode().strip(),
                            hmac=base64.encodebytes(hmac).decode().strip())
--- app/api/settings.py
+++ app/api/settings.py
@@ -6,4 +6,4 @@ bp = Blueprint('api.settings', __name__, url_prefix='/api/settings')
 @bp.route('/')
 def get_settings():
     state = current_app.config['INIT_STATE']
-    return jsonify(state='%d' % state)
+    return jsonify(state=state)
