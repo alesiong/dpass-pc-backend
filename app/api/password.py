5d301a2faf6e53ca15bc3300b728a0e7fd49f005
Yuhengli30 <731894111@qq.com>
2018-03-08 14:40:34 +0800

    add function "new"

--- app/api/password.py
+++ app/api/password.py
@@ -1,3 +1,4 @@
+import random
 import base64
 import binascii
 from flask import Blueprint, current_app, jsonify, request, json
@@ -8,7 +9,7 @@ from app.utils import error_respond
 from app.utils.cipher import encrypt_and_authenticate
 from app.utils.decorators import session_verify
 from app.utils.master_password import MasterPassword
-
+from app.utils.local_storage import LocalStorage
 bp = Blueprint('api.password', __name__, url_prefix='/api/password')
 
 
@@ -38,4 +39,25 @@ def get_table():
     data, hmac = encrypt_and_authenticate(json.dumps(entries).encode(),
                                           binascii.unhexlify(key))
     return jsonify(data=base64.encodebytes(data).decode().strip(),
+                   hmac=base64.encodebytes(hmac).decode().strip())
+
+@bp.route('/new/', methods=['POST'])
+@session_verify
+def new():
+    key = SessionKey().session_key
+    master_password: MasterPassword = current_app.config['MASTER_PASSWORD']
+    expired = master_password.check_expire()
+    if expired:
+        error_respond.authentication_failure()
+    entries = KeyLookupTable.query.all()
+    new_key = random.random()
+    while KeyLookupTable.query.filter_by(key=new_key).count() != 0:
+        new_key = random.random()
+    entry = json.dumps(request.decrypted_data.decode())
+    KeyLookupTable.new_entry(new_key, entry.encode())
+    current_app.config['STORAGE'].add(new_key, entry.encode())
+
+    new_key, hmac = encrypt_and_authenticate(json.dumps(new_key).encode(),
+                                          binascii.unhexlify(key))
+    return jsonify(key=base64.encodebytes(new_key).decode().strip(),
                    hmac=base64.encodebytes(hmac).decode().strip())
