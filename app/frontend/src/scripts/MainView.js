781fc97e297bbc5ff3760cc3f059d5276856fe0a
alesiong <ypf7.10@hotmail.com>
2018-03-08 13:47:13 +0800

    extract out the password dialog

--- app/frontend/src/scripts/MainView.js
+++ app/frontend/src/scripts/MainView.js
@@ -1,111 +1,102 @@
-import {encryptAndAuthenticate} from '../utils';
-import mdui from 'mdui';
+import PasswordDialog from '@c/PasswordDialog';
 
 export default {
   name: 'main-view',
+  components: {
+    PasswordDialog
+  },
   data() {
     return {
       url: '',
-      siteName:'',
+      siteName: '',
       userId: '',
       password: '',
-      items:[
+      items: [
         {
-          url:'http://www.amazon.com',
-          siteName:'Amazon',
-          userId:'JoeStalin',
-          date:'2018/3/7',
-          hide:false
+          url: 'http://www.amazon.com',
+          siteName: 'Amazon',
+          userId: 'JoeStalin',
+          date: '2018/3/7',
+          hide: false
         },
         {
-          url:'http://www.taobao.com',
-          siteName:'Taobao',
-          userId:'StFrank',
-          date:'2018/3/7',
-          hide:false
+          url: 'http://www.taobao.com',
+          siteName: 'Taobao',
+          userId: 'StFrank',
+          date: '2018/3/7',
+          hide: false
         }
       ]
     };
   },
   mounted() {
-    this.onAddPassword();
+    this.$refs.dialog.openDialog();
   },
 
   methods: {
-    checkIsInvalid() {
-      return !(this.url !== '' && this.userId !== '' && this.password !== '' && this.siteName !== '');
-    },
-    addItem() {
-      const urlTest = '';
-      console.log(this.url);
-      // if (!this.checkIsInvalid()) {
-      //   new mdui.Dialog('#AddDialog').close();
-      // }
+    addItem(data) {
+      console.log(data);
     },
     onAddPassword() {
-      this.url = '';
-      this.userId = '';
-      this.password = '';
-      this.siteName='';
-      new mdui.Dialog('#AddDialog',{modal:true}).open();
-    },
-    addPassword: function() {
-      mdui.prompt('Enter master password:', this.onAdd.bind(this),
-          () => {
-          }, {
-            modal: true,
-            histroy: false // TODO: remember to add this, or vue's router may fail
-          });
-    },
-    verifyPassword: function() {
-      mdui.prompt('Verify master password:', this.onVerify.bind(this),
-          () => {
-          }, {
-            modal: true,
-            histroy: false
-          });
-    },
-    onAdd: function(value) {
-      const [cipher, hmac] = encryptAndAuthenticate(value, this.globalData.sessionKey);
-      $$.ajax({
-        url: '/api/master_password/new/',
-        method: 'POST',
-        dataType: 'json',
-        data: JSON.stringify({
-          data: cipher,
-          hmac: hmac
-        }),
-        contentType: 'application/json',
-        success: () => {
-          mdui.alert('Master Password Set');
-        },
-        statusCode: {
-          '401': () => {
-            mdui.alert('Session Key broken!');
-          }
-        }
-      });
-    },
-    onVerify: function(value) {
-      const [cipher, hmac] = encryptAndAuthenticate(value, this.globalData.sessionKey);
-      $$.ajax({
-        url: '/api/master_password/verify/',
-        method: 'POST',
-        dataType: 'json',
-        data: JSON.stringify({
-          data: cipher,
-          hmac: hmac
-        }),
-        contentType: 'application/json',
-        success: () => {
-          mdui.alert('Master Password Verified');
-        },
-        statusCode: {
-          '401': () => {
-            mdui.alert('Verification failed or Session Key broken!');
-          }
-        }
-      });
+      this.$refs.dialog.openDialog();
     }
+    // addPassword: function() {
+    //   mdui.prompt('Enter master password:', this.onAdd.bind(this),
+    //       () => {
+    //       }, {
+    //         modal: true,
+    //         histroy: false // TODO: remember to add this, or vue's router may fail
+    //       });
+    // },
+    // verifyPassword: function() {
+    //   mdui.prompt('Verify master password:', this.onVerify.bind(this),
+    //       () => {
+    //       }, {
+    //         modal: true,
+    //         histroy: false
+    //       });
+    // },
+    // onAdd: function(value) {
+    //   const [cipher, hmac] = encryptAndAuthenticate(value, this.globalData.sessionKey);
+    //   $$.ajax({
+    //     url: '/api/master_password/new/',
+    //     method: 'POST',
+    //     dataType: 'json',
+    //     data: JSON.stringify({
+    //       data: cipher,
+    //       hmac: hmac
+    //     }),
+    //     contentType: 'application/json',
+    //     success: () => {
+    //       mdui.alert('Master Password Set');
+    //     },
+    //     statusCode: {
+    //       '401': () => {
+    //         mdui.alert('Session Key broken!');
+    //       }
+    //     }
+    //   });
+    // },
+    // onVerify: function(value) {
+    //   const [cipher, hmac] = encryptAndAuthenticate(value, this.globalData.sessionKey);
+    //   $$.ajax({
+    //     url: '/api/master_password/verify/',
+    //     method: 'POST',
+    //     dataType: 'json',
+    //     data: JSON.stringify({
+    //       data: cipher,
+    //       hmac: hmac
+    //     }),
+    //     contentType: 'application/json',
+    //     success: () => {
+    //       mdui.alert('Master Password Verified');
+    //     },
+    //     statusCode: {
+    //       '401': () => {
+    //         mdui.alert('Verification failed or Session Key broken!');
+    //       }
+    //     }
+    //   });
+    // }
   }
 };
---
+++ app/frontend/src/scripts/PasswordDialog.js
@@ -0,0 +1,35 @@
+import mdui from 'mdui'
+
+export default {
+  name: 'password-dialog',
+  props: ['initUrl', 'initSiteName', 'initUserId', 'initTitle'],
+  data() {
+    return {
+      url: this.initUrl || '',
+      siteName: this.initSiteName || '',
+      userId: this.initUserId || '',
+      password: '',
+      title: this.initTitle || 'Enter New Password',
+      id: `password-dialog-${this._uid}`
+    };
+  },
+  computed: {
+    valid() {
+      return this.url !== '' && this.userId !== '' && this.password !== '' && this.siteName !== '';
+    }
+  },
+  methods: {
+    openDialog(options) {
+      options = Object.assign({modal: true}, options);
+      new mdui.Dialog('#' + this.id, options).open();
+    },
+    onClickAdd() {
+      this.$emit('click-add', {
+        url: this.url,
+        siteName: this.siteName,
+        userId: this.userId,
+        password: this.password
+      });
+    }
+  }
+};
