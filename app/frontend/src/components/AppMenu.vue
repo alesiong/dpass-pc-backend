dd757d3f66ed2cbf23f68b34a2a4a1d84b49a67a
zymop18 <115010276@link.cuhk.edu.cn>
2018-03-12 20:19:17 +0800

    add some contents

--- app/frontend/src/components/AppMenu.vue
+++ app/frontend/src/components/AppMenu.vue
@@ -1,4 +1,37 @@
 <template>
+    <!--TODO: here are the navigation menu-->
+    <div class="mdui-drawer mdui-color-theme-800" id="left-drawer">
+        <ul class="mdui-list" id="menu">
+            <router-link class="mdui-list-item mdui-ripple"
+                         to="/"
+                         exact-active-class="mdui-list-item-active">
+                All
+            </router-link>
+            <router-link class="mdui-list-item mdui-ripple"
+                         to="/password/"
+                         exact-active-class="mdui-list-item-active">
+                Passwords
+            </router-link>
+            <router-link class="mdui-list-item mdui-ripple"
+                         to="/secretNote/"
+                         exact-active-class="mdui-list-item-active">
+                Secret Notes
+            </router-link>
+            <li class="mdui-divider mdui-color-white"  ></li>
+            <li class="mdui-menu-item mdui-ripple"style="margin-top:2.5em" >
+                <i class="mdui-menu-item-icon mdui-icon material-icons">settings_backup_restore
+                </i>Sync & Backup
+            </li>
+            <li class="mdui-menu-item mdui-ripple"style="margin-top:1.0em">
+                <i class="mdui-menu-item-icon mdui-icon material-icons">settings</i>Setting
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<!--<template>
     <div class="mdui-drawer mdui-color-theme-800" id="left-drawer">
         <ul class="mdui-list" id="menu">
             <router-link class="mdui-list-item mdui-ripple"
@@ -16,7 +49,7 @@
             </li>
         </ul>
     </div>
-</template>
+</template>-->
 
 <script>
   export default {
